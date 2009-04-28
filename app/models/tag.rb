e1de804ede03cf5b5b249ed882cec11c2b080fe1
André Lynum <stinky@1x-193-157-199-87.uio.no>
2009-04-28 11:34:09 +0200

    Added methods for extracting tag strings and cleaned up tag strings.

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,3 +1,14 @@
 class Tag < ActiveRecord::Base
   belongs_to :word
+  @@clean_tag_regex = Regexp.compile('((i|pa|tr|pr|r|rl|a|d|n)\d+(\/til)?)')
+
+  public
+    
+  def out_tag()
+    self.string.strip.gsub(/\s+/, '_')
+  end
+
+  def clean_out_tag()
+    self.string.gsub(@@clean_tag_regex, '').strip.gsub(/\s+/, '_')
+  end
 end
