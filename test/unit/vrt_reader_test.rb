f2567daeadea5991189f1dbf45d91223a295adb0
Andre Lynum <and-ly@online.no>
2013-02-12 07:43:36 +0100

    Simple test for preserving sgml data in VRT files.

--- test/unit/vrt_reader_test.rb
+++ test/unit/vrt_reader_test.rb
@@ -16,9 +16,32 @@ class VRTReaderTest < Test::Unit::TestCase
     assert_equal(3, text.sentences[1].length)
 
     out = VRTReader.textString(text)
-    out.tr_s("\t ", ' ').strip!
+    out = out.tr_s("\t ", ' ').strip!
     out_file = File.open(fn).read
-    out_file.tr_s("\t ", ' ').strip!
+    out_file = out_file.tr_s("\t ", ' ').strip!
+
+    assert_equal(out_file, out)
+  end
+
+  def test_tt_sgml
+    fn = File.join(TEST_DATA_PATH, 'tt_sgml_1')
+
+    # TODO build this into the parser interfaces
+    text = TaggedText.new
+    iterator = VRTReader.new(File.open(fn))
+    iterator.each_sentence { |s| text.sentences << s}
+    text.save
+
+    text.postamble = iterator.postamble.join('\n')
+
+    assert_equal(2, text.sentences.count)
+    assert_equal(5, text.sentences[0].length)
+    assert_equal(3, text.sentences[1].length)
+
+    out = VRTReader.textString(text)
+    out = out.tr_s("\t ", ' ').strip
+    out_file = File.open(fn).read
+    out_file = out_file.tr_s("\t ", ' ').strip
 
     assert_equal(out_file, out)
   end
